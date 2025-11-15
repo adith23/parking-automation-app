c9ecdbd5677d9f75f162529209084a36e2b3d205
adith23 <129499315+adith23@users.noreply.github.com>
2025-11-15 15:28:33 +0530

    Update 14

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,9 +1,9 @@
-    # Import all models here so that Base has them registered
-    from app.models.driver_models.booking_model import Booking
-    from app.models.driver_models.driver_model import Driver
-    from app.models.driver_models.parking_session_model import ParkingSession
-    from app.models.driver_models.vehicle_model import Vehicle
-    from app.models.owner_models.owner_model import ParkingLotOwner
-    from app.models.owner_models.parking_lot_model import ParkingLot
-    from app.models.owner_models.parking_slot_model import ParkingSlot, SlotDefinition
-    from app.models.owner_models.subscription_model import SubscriptionPlan, OwnerSubscription
+# Import all models here so that Base has them registered
+from app.models.driver_models.booking_model import Booking
+from app.models.driver_models.driver_model import Driver
+from app.models.driver_models.parking_session_model import ParkingSession
+from app.models.driver_models.vehicle_model import Vehicle
+from app.models.owner_models.owner_model import ParkingLotOwner
+from app.models.owner_models.parking_lot_model import ParkingLot
+from app.models.owner_models.parking_slot_model import ParkingSlot, SlotDefinition
+from app.models.owner_models.subscription_model import SubscriptionPlan, OwnerSubscription
